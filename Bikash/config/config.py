0f1d02428b71df1e7231d22861fc67bb326371e6
Bossy325 <155837017+Bossy325@users.noreply.github.com>
2024-01-12 23:11:01 +0600

    Update config.py

--- Bikash/config/config.py
+++ Bikash/config/config.py
@@ -9,13 +9,13 @@ from pyrogram import filters
 if os.path.exists("Internal"):
   load_dotenv("Internal")
 
-API_ID = int(getenv("API_ID" ""))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID","26744258"))
+API_HASH = getenv("API_HASH","7c7dbcb9e6654389d112b418e10583fa")
+BOT_TOKEN = getenv("BOT_TOKEN","6516362701:AAFwYh-zvb8dtdn_trPMJXJ6uYmfj3E_S4M")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༺ 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ༻")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Saib:saib2005@cluster0.1l0w1wa.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1002023679721"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Url shortner bot ")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6696466266").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
@@ -47,7 +47,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 SET_CMDS = getenv("SET_CMDS", False)
 # https://www.gbmb.org/mb-to-bytes
 
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION","BQGYFcIAjVetK2ocLWlu_obBa174JBZdmPdaSyCOYeT4yGDg4TgkiSCV6EXLuQZadXOzWgOP8mjHX5wjzdMRQ6Pd5x70pFf_3QlrY9LDt-_Tv25bG4aAOQkdSLJBsJAA9IlyvJYbpBg-vLI8ms4YODBCyA7EvHG34rKEM2KO6Ykj4B9qpBw81X3z8e47D2luVWcGcU0VZ-yj2T-Vi0zoTDGSwNB820Xdf_K-kw8XVmzme6iflEJws9gTS3C8N39OMRHIDQVw_EAnDV2cquqwtGViOJYIx1I9XDeN2lzB05OKMYWfYaXgXVKFXesd0XK1w5xlqwWgydPqtoxDsyiBlg6pH_EPLgAAAAGPI_daAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -55,7 +55,7 @@ STRING5 = getenv("STRING_SESSION5", None)
 
 ############################
 COMMAND_PREFIXES.append('')
-OWNER_ID.append(1439222689)
+OWNER_ID.append(6696466266)
 ############################
 BANNED_USERS = filters.user()
 YTDOWNLOADER = 1
