6eaa5debb09299ad68c4254287d452b8c772a480
Bossy325 <155837017+Bossy325@users.noreply.github.com>
2024-01-12 14:03:20 +0600

    Update config.py

--- Bikash/config/config.py
+++ Bikash/config/config.py
@@ -16,14 +16,14 @@ COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༺ 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ༻")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1439222689").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6696466266").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BikashHalderNew/Bgtplayer")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "bikash")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BikashGadgetsTech")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Bgt_Chat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/holaien")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/holaien")
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "180"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
@@ -69,20 +69,20 @@ clean = {}
 autoclean = []
 ############################
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/God-01-09-9")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/3127b3484676bd2254e97.jpg")
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
-GLOBAL_IMG_URL = "https://te.legra.ph/file/2e2741f5dfe9f62eed91d.png"
-STATS_IMG_URL = "https://te.legra.ph/file/2be94fc76030833b51a78.png"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-STREAM_IMG_URL = "https://graph.org/file/93882ae5ea01a7bf687b1.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/0d021735560cbf0bb749a.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/4a8cc770a5bea2136bada.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/Joke-01-09"
+GLOBAL_IMG_URL = "https://telegra.ph/Hi-01-05-162"
+STATS_IMG_URL = "https://telegra.ph/God-01-09-9"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/Hi-01-05-162"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/Hi-01-05-162"
+STREAM_IMG_URL = "https://telegra.ph/God-01-09-9"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/Hi-01-05-162"
+YOUTUBE_IMG_URL = "https://telegra.ph/Joke-01-09"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/Hi-01-05-162"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/Joke-01-09"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/God-01-09-9"
 
 
 def time_to_seconds(time):
@@ -113,4 +113,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "resources/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
+            START_IMG_URL = "https://telegra.ph/God-01-09-9"
