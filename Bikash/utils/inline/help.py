eb42306a2537796ea22f7286c334c4f4915e82c3
Bossy325 <155837017+Bossy325@users.noreply.github.com>
2024-01-12 23:01:30 +0600

    Update help.py

--- Bikash/utils/inline/help.py
+++ Bikash/utils/inline/help.py
@@ -13,11 +13,11 @@ def help_pannel(_, START: Union[bool, int] = None):
         InlineKeyboardButton(
             # text=_["BACK_BUTTON"],
             text="🏠 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🏠",
-            url=f"https://t.me/BikashGadgetsTech",
+            url=f"https://t.me/holaien",
         ),
         InlineKeyboardButton(
             text="𝐂𝐫𝐞𝐚𝐭𝐨𝐫",
-            url=f"t.me/BikashHalder",
+            url=f"t.me/Goatijo",
         ),
         InlineKeyboardButton(
             text="𝐂𝐥𝐨𝐬𝐞", callback_data=f"close"
